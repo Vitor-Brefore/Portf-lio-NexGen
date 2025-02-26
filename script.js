12fce6c9ea4809237df6fb91c754af82ad49aa66
Vitor-Brefore <vitorbrefore93@gmail.com>
2025-02-26 14:43:07 -0300

    msg

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", function () {
   setTimeout(() => {
     const loadingScreen = document.getElementById("loading-screen")
-    loadingScreen.style.display = "none"
+    loadingScreen.style.opacity = "0"
+    loadingScreen.style.zIndex = "-1"
 
     const conteudo = document.getElementById("conteudo")
     conteudo.style.display = "block"
@@ -10,76 +11,76 @@ window.addEventListener("load", function () {
 
 window.revelar = ScrollReveal({ reset: true })
 
-revelar.reveal('.inv', {
+revelar.reveal(".inv", {
   duration: 1500,
-  distance: '90px'
+  distance: "90px",
 })
 
-revelar.reveal('.invtit1', {
+revelar.reveal(".invtit1", {
   duration: 1500,
-  distance: '100px'
+  distance: "100px",
 })
 
-revelar.reveal('.invtit2', {
+revelar.reveal(".invtit2", {
   duration: 1500,
-  distance: '150px',
-  delay: 500
+  distance: "150px",
+  delay: 500,
 })
 
-revelar.reveal('.invtit3', {
+revelar.reveal(".invtit3", {
   duration: 1500,
-  distance: '200px',
-  delay: 1000
+  distance: "200px",
+  delay: 500,
 })
 
-revelar.reveal('.invtit4', {
+revelar.reveal(".invtit4", {
   duration: 1500,
-  distance: '250px',
-  delay: 1500
+  distance: "250px",
+  delay: 1000,
 })
 
-revelar.reveal('.invp2-1', {
+revelar.reveal(".invp2-1", {
   duration: 1500,
-  distance: '100px'
+  distance: "100px",
 })
 
-revelar.reveal('.invp2-2', {
+revelar.reveal(".invp2-2", {
   duration: 1500,
-  distance: '150px',
-  delay: 500
+  distance: "150px",
+  delay: 500,
 })
 
-revelar.reveal('.invp2-3', {
+revelar.reveal(".invp2-3", {
   duration: 1500,
-  distance: '200px',
-  delay: 1000
+  distance: "200px",
+  delay: 1000,
 })
 
-revelar.reveal('.invp2-4', {
+revelar.reveal(".invp2-4", {
   duration: 1500,
-  distance: '250px',
-  delay: 1500
+  distance: "250px",
+  delay: 1500,
 })
 
-revelar.reveal('.invp3-1', {
+revelar.reveal(".invp3-1", {
   duration: 1500,
-  distance: '100px'
+  distance: "100px",
 })
 
-revelar.reveal('.invp3-2', {
+revelar.reveal(".invp3-2", {
   duration: 1500,
-  distance: '150px',
-  delay: 500
+  distance: "150px",
+  delay: 500,
 })
 
-revelar.reveal('.invp3-3', {
+revelar.reveal(".invp3-3", {
   duration: 1500,
-  distance: '200px',
-  delay: 1000
+  distance: "200px",
+  delay: 1000,
 })
 
-revelar.reveal('.invp3-4', {
+revelar.reveal(".invp3-4", {
   duration: 1500,
-  distance: '250px',
-  delay: 1500
+  distance: "250px",
+  delay: 1500,
 })
